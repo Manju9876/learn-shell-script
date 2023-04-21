4443e9c37ebfc380b113618fbb93055dcc5ca1cb
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-22 03:20:07 +0530

    source common code 1

--- example_problems.sh
+++ example_problems.sh
@@ -2,20 +2,40 @@
 # > 7 - Good
 # < 7 - Average
 
-input=$1
+#input=$1
+#
+#if [ -z "$input" ]
+#then
+#  echo input movie name
+#  exit
+#
+#fi
+#percentage=$(curl -s  https://www.themoviedb.org/movie/$input | grep data-percent| xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
+#
+#echo percent is -$percentage
+#
+#if [ "$percentage" -ge 70 ]
+#then
+#  echo the movie ratting is Good
+#else
+#  echo the movie ratting is Average
+#fi
 
+#get the ratting
+# > 75 good
+# < 75 average
+
+input=$1
 if [ -z "$input" ]
 then
-  echo input movie name
-  exit
+  echo please provide the movie url
 fi
-percentage=$(curl -s  https://www.themoviedb.org/movie/$input | grep data-percent| xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
-echo percent is -$percentage
+percent=$(curl -s  https://www.rottentomatoes.com/m/evil_dead_rise | grep -w 86 | xargs -0 -n1 | grep  audiencescore | awk -F = '{print $2}')
 
-if [ "$percentage" -ge 70 ]
+if [ "$percent" -ge 75 ]
 then
-  echo the movie ratting is Good
+  echo the movie is Good
 else
-  echo the movie ratting is Average
+  echo the movie is Average
 fi

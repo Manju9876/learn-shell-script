d9119375b54f025751469d8fcfd2b80117fb2eaf
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-22 01:49:33 +0530

    source common code 1

--- if.sh
+++ if.sh
@@ -1,5 +1,9 @@
 fruit_name=$1
 quantity=$2
+if [ -z "$fruit_name" ]
+then
+  echo input missing
+  fi
 
 if [ "${fruit_name}" == "mango" ]
  then
@@ -23,3 +27,15 @@ then
 else
   echo the fruit per piece is - 1.00$
 fi
+
+student_name=$1
+result_pass=$2
+result_fail=$3
+
+if [ "$student_name" == "manju" ]
+then
+  echo sutden result is - $result_pass
+else
+  echo  studen result is - $result_fail
+fi
+

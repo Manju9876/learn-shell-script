86758fe293bbf6c563546f9d177ca6c7f092ad74
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2023-04-23 22:28:53 +0530

    source common code

---
+++ case.sh
@@ -0,0 +1,15 @@
+
+
+student_details=$1
+
+case ${student_details} in
+   manju)
+     echo -e "\e[36m scored average marks of 75\e[0m"
+    ;;
+  sundar)
+    echo -e "\e[36m scored average marks of  95\e[0m"
+    ;;
+  parmesh)
+    echo -e "\e[36m scored average marks of  60\e[0m"
+    ;;
+   esac
